6223049c316efc86edd8c85e4ebb82b19d43e1ca
Valentin <107526652+Valenchongo@users.noreply.github.com>
2023-08-01 22:01:47 -0300

    jero_nueva_pricncipal

---
+++ funciones.js
@@ -0,0 +1,7 @@
+boton = document.querySelector(".hamburger");
+mobile = document.querySelector(".div_mobile")
+
+boton.addEventListener("click",()=>{
+    mobile.classList.toggle("div_mobiledespleged")
+
+})
